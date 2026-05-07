fef38e348e25c9709bdd88e4853134d1aa7447cc
Thai Auto conversion <ponchok.k@thaiautoconv.com>
2026-05-07 10:50:14 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -29,6 +29,7 @@ const portalData = [
                 title: 'บริการความช่วยเหลือ (IT Support)',
                 links: [
                     { title: 'ระบบแจ้งซ่อม IT (Helpdesk System)', url: `https://apps.powerapps.com/play/e/default-d7cb89dc-f748-4a69-9ffc-7dc54794aff5/a/9d77669f-bebd-42f9-a86e-db439684169e?source=teamsopenwebsite&screenColor=rgba(0%2C%20176%2C%20240%2C%201)&hint=362c3461-6fd1-4cb6-84be-da419a0e8abd&tenantId=d7cb89dc-f748-4a69-9ffc-7dc54794aff5`, type: 'web' },
+                    { title: 'เบอร์ติดต่อภายในองค์กร', url: 'https://thaiauto.sharepoint.com/:b:/r/sites/it/Shared%20Documents/IT%20Application/Aek%20Test/Test%20new/TAC%20Internal%20Contacts%20Rv.02.pdf?csf=1&web=1&e=MDjcuG', type: 'pdf' },
                 ]
             },
             {
