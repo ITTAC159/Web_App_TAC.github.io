bb811ab51645a7887ef1c8de9efff95250759a1e
Thai Auto conversion <ponchok.k@thaiautoconv.com>
2026-05-08 11:57:47 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -10,11 +10,7 @@ const portalData = [
         description: 'ข้อมูลสวัสดิการ กฎระเบียบ และระบบสำหรับพนักงาน',
         categories: [
             {
-                title: 'คู่มือและนโยบาย',
-                links: []
-            },
-            {
-                title: 'ระบบออนไลน์ (E-Services)',
+                title: 'ระบบออนไลน์ (E-Services) HR',
                 links: []
             }
         ]
@@ -29,13 +25,10 @@ const portalData = [
                 title: 'บริการความช่วยเหลือ (IT Support)',
                 links: [
                     { title: 'ระบบแจ้งซ่อม IT (Helpdesk System)', url: `https://apps.powerapps.com/play/e/default-d7cb89dc-f748-4a69-9ffc-7dc54794aff5/a/9d77669f-bebd-42f9-a86e-db439684169e?source=teamsopenwebsite&screenColor=rgba(0%2C%20176%2C%20240%2C%201)&hint=362c3461-6fd1-4cb6-84be-da419a0e8abd&tenantId=d7cb89dc-f748-4a69-9ffc-7dc54794aff5`, type: 'web' },
-                    { title: 'เบอร์ติดต่อภายในองค์กร', url: 'https://thaiauto.sharepoint.com/:b:/r/sites/it/Shared%20Documents/IT%20Application/Aek%20Test/Test%20new/TAC%20Internal%20Contacts%20Rv.02.pdf?csf=1&web=1&e=MDjcuG', type: 'pdf' },
+                    { title: 'Intranet IT', url: 'https://thaiauto.sharepoint.com/:f:/s/test/IgD18rwC4cbCT7ls2LyhJrCrASih1IuAAhUgH78eFAR9vsw', type: 'web' },
                 ]
             },
-            {
-                title: 'ซอฟต์แวร์และนโยบายความปลอดภัย',
-                links: []
-            }
+
         ]
     },
     {
@@ -45,11 +38,7 @@ const portalData = [
         description: 'บริการส่วนกลาง อาคารสถานที่ และยานพาหนะ',
         categories: [
             {
-                title: 'บริการส่วนกลาง',
-                links: []
-            },
-            {
-                title: 'แบบฟอร์มและระเบียบปฏิบัติ',
+                title: 'ระบบออนไลน์ (E-Services) GA',
                 links: []
             }
         ]
@@ -61,11 +50,7 @@ const portalData = [
         description: 'มาตรฐานความปลอดภัย อาชีวอนามัย และสิ่งแวดล้อม',
         categories: [
             {
-                title: 'มาตรฐานและความปลอดภัย (OH&S)',
-                links: []
-            },
-            {
-                title: 'รายงานและการจัดการ',
+                title: 'ระบบออนไลน์ (E-Services) Safty',
                 links: []
             }
         ]
